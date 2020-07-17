8f51eb0303241aa70055aa17c8940dfd359ea8a4
Abhishek Kumar <abhishek022kk@gmail.com>
2020-07-17 14:43:17 +0530

    Mini Project

---
+++ src/components/counter.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button
+          className="btn btn-secondary btn-sm m-2"
+          onClick={(product) => this.handleIncrement(product)}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 2 },
+      { id: 3, value: 4 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = counterId => {
+    // const counters = this.state.counters.filter((c) => c.id !== counterId);
+    // this.setState({ counters });
+    console.log("Clicked", counterId);
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            onDelete={this.handleDelete}
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
