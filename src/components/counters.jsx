74d1c9eee68a791962c22c782be4e83029467b69
Abhishek Kumar <abhishek022kk@gmail.com>
2020-07-18 01:10:51 +0530

    Destructuring Elements

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,51 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 2 },
-      { id: 3, value: 4 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm btn-2"
         >
           Reset
         </button>
 
-        {this.state.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
-            onDelete={this.handleDelete}
+            onDelete={onDelete}
             key={counter.id}
-            onIncrement={this.handleIncrement}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
